30c184c354f39e24ec5daf9feb085488f2181c36
Gustavo Hillesheim Teixeira <g.hillesheim@edu.pucrs.br>
2025-10-03 15:50:59 -0300

    starting user form

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.scss'
 })
 export class RegistroComponent {
+  protected usuarioForm : FormGroup;
+  constructor(private fb: FormBuilder){
+    this.usuarioForm = this.fb.group({
+      primeiroNome : [null, Validators.required, Validators.pattern('[a-zA-Z ]*')],
+      sobrenome : [null, Validators.required, Validators.pattern('[a-zA-Z ]*')]
+      
+    })
+  }
 
 }
