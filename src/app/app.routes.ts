4ba75de9388fda3c17a021ea136ae0e1d8936a00
Gustavo Hillesheim <g.hillesheim@edu.pucrs.br>
2025-10-10 11:37:07 -0300

    develop rebased

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegistroComponent } from './pages/registro/registro.component';
-import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'registro', component: RegistroComponent},
+    { path: 'home', component: PrincipalComponent},
     { path: 'login', component: LoginComponent},
     { path: '**', redirectTo: '' } 
 ];
