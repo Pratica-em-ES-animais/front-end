6d0fb3845daa2c892e347a816e78ea8081616789
Gustavo Hillesheim <g.hillesheim@edu.pucrs.br>
2025-10-09 21:27:20 -0300

    login done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'registro', component: RegistroComponent},
+    { path: 'login', component: LoginComponent},
     { path: '**', redirectTo: '' } 
 ];
