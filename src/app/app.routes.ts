ba4b70667fa31fe125c7f7069ba57371a715e0e1
Gustavo Hillesheim <g.hillesheim@edu.pucrs.br>
2025-10-10 12:14:39 -0300

    pagina principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
